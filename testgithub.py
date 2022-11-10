64b425a0617ffe44e3cc3c4ce448e5f933865338
gurasho <tobineko777@gmail.com>
2022-11-11 07:17:45 +0900

    new file

---
+++ testgithub.py
@@ -0,0 +1,28 @@
+#https://blog.imind.jp/entry/2020/01/18/065638
+import webbrowser,sys,os
+import git
+
+url = 'https://github.com/gurasho/pythonmemo.git'
+to_path = 'foo'
+if not os.path.exists('foo'):
+    git.Repo.clone_from(
+        url,
+        to_path
+    )
+
+with open('foo/testgithub.py', 'w') as fp:
+    with open('testgithub.py', 'r', encoding='utf-8') as fp2:
+        fp.write(fp2.read())
+        fp2.close()
+        fp.close()
+
+repo = git.Repo(to_path)
+repo.git.checkout('new_branch')
+ufiles = repo.untracked_files
+for f in ufiles:
+    print(ufiles)
+    repo.git.add(f)
+    repo.git.commit(f, message='new file', author='gurasho')
+repo.git.push('origin', 'new_branch')
+print(repo.git.branch() )
+
